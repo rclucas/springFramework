bfae637b22927dead2f5b287428c32bd42fe7add
rodrigo <rodrigo@sonner.com.br>
2025-11-27 15:17:10 -0300

    converter dados

--- src/main/java/br/com/rcl/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/rcl/screenmatch/service/ConverteDados.java
@@ -1,12 +1,17 @@
 package br.com.rcl.screenmatch.service;
 
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverteDados implements IConverteDados{
     private ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
-        return mapper.readValue(json, classe);
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
